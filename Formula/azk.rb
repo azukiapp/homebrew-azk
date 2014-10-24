20ed8b1440c2c1838d427b5382253877a091bb51
Everton Ribeiro <nuxlli@gmail.com>
2014-10-24 13:07:48 -0200

    Removing version (will be extracted from the url)

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -3,7 +3,6 @@ require "formula"
 class Azk < Formula
   homepage "http://azk.io"
   url "https://s3-sa-east-1.amazonaws.com/azk/azk_0.5.1.tar.gz"
-  version "0.5.1"
   sha256 "23ea48d4d11025813e2b1ebea7fe9ae02c4747375dacd54f4bc636dee1728406"
 
   depends_on :macos => :mountain_lion
