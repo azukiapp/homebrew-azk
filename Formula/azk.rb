5dea1ecd187c1743041dafd030034e6b7caef6ba
Everton Ribeiro <nuxlli@gmail.com>
2014-10-30 02:58:31 -0200

    Updating shasum

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -3,7 +3,7 @@ require "formula"
 class Azk < Formula
   homepage "http://azk.io"
   url "http://repo.azukiapp.com/mac/azk_0.6.0.tar.gz"
-  sha256 "fd402fc2a9969aafacf148096eb16087724f482e8ccf233287baaf8cefca8082"
+  sha256 "4c31899f46ff7255174426bfd6206d8b882bd5b5bac9b4d5b24e61d52e935b2b"
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
