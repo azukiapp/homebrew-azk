f145ed0c8f722f4abe1d8fd76c13d3a394e8f6c4
Everton Ribeiro <nuxlli@gmail.com>
2014-11-18 19:14:26 -0200

    Upgrading v0.7.0

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Azk < Formula
   homepage "http://azk.io"
-  url "http://repo.azukiapp.com/mac/azk_0.6.1.tar.gz"
-  sha256 "5fefbdf9b14aea62658a4751a15865a8a185807fd1ee531b8d82df9418f1d6f8"
+  url "http://repo.azukiapp.com/mac/azk_0.7.0.tar.gz"
+  sha256 "01879a1261de59a8cc4f3c763bb27b7a6049bb7144613ad23b44dbbbd97b6297"
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
