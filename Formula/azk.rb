656e1e3bdcc82b31c21c864c00393af2ddbc334b
Gullit Miranda <gullitmiranda@gmail.com>
2015-01-28 18:04:50 -0200

    Bumped version number to azk 0.9.1

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Azk < Formula
   homepage "http://azk.io"
-  url "http://repo.azukiapp.com/mac/azk_0.9.0.tar.gz"
-  sha256 "36d6eb426167df77c5140299ee9c2325ff7d171fe49fa0936f8265e2ad912093"
+  url "http://repo.azukiapp.com/mac/azk_0.9.1.tar.gz"
+  sha256 "01790ba04d3bfdb263c059f582a7aa466bce2d2aba732a4112999e4d49ea974c"
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
