78da75b7c9674232d186f2b54213293a066d9c1f
Everton Ribeiro <nuxlli@gmail.com>
2015-04-27 12:36:20 -0300

    Moving 0.12.1 to stable

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Azk < Formula
   homepage "http://azk.io"
-  url "http://repo-stage.azukiapp.com/mac/azk_0.12.1.tar.gz"
+  url "http://repo.azukiapp.com/mac/azk_0.12.1.tar.gz"
   sha256 "13274fcf1f135ef3a1f8beb2c0ca42648f0dbe92b7665c7d8e560f9bc834bd9b"
 
   depends_on :macos => :mountain_lion
