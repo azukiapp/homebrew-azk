9646e66dc3aa3ca03bcd8d82d45263b8fc87a581
Gullit Miranda <gullitmiranda@gmail.com>
2015-05-21 17:02:30 -0300

    Adding devel build

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -5,6 +5,12 @@ class Azk < Formula
   url "http://repo.azukiapp.com/mac/azk_0.12.1.tar.gz"
   sha256 "13274fcf1f135ef3a1f8beb2c0ca42648f0dbe92b7665c7d8e560f9bc834bd9b"
 
+  devel do
+    url "http://repo.azukiapp.com/mac/azk_0.13.0-beta1.tar.gz"
+    sha256 "e475ef1ee66f417c93259273286ebbc77fd53f522a9c5d2fbf1676bebe4034ad"
+    version "v0.13.0-beta1"
+  end
+
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
 
