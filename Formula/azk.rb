794cdee99e36e30731b935f848cccad0f82884c7
Everton Ribeiro <nuxlli@azukiapp.local>
2015-05-27 11:23:52 -0300

    Fixing sha256 from 0.13.0 stage version

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -3,7 +3,7 @@ require "formula"
 class Azk < Formula
   homepage "http://azk.io"
   url "http://repo-stage.azukiapp.com/mac/azk_0.13.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "f8d0a4b02c983de8a703b25480abcfc3224a0b86613b4a7ce53316657efbaa7a"
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
