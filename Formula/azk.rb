af66a8e0d2c9c97346d529b121c929b722a9dd76
Robson Peixoto <robsonpeixoto@gmail.com>
2015-05-27 23:44:07 -0300

    azk 0.13.0

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -2,14 +2,8 @@ require "formula"
 
 class Azk < Formula
   homepage "http://azk.io"
-  url "http://repo.azukiapp.com/mac/azk_0.12.1.tar.gz"
-  sha256 "13274fcf1f135ef3a1f8beb2c0ca42648f0dbe92b7665c7d8e560f9bc834bd9b"
-
-  devel do
-    url "http://repo.azukiapp.com/mac/azk_0.13.0-beta1.tar.gz"
-    sha256 "e475ef1ee66f417c93259273286ebbc77fd53f522a9c5d2fbf1676bebe4034ad"
-    version "v0.13.0-beta1"
-  end
+  url "http://repo.azukiapp.com/mac/azk_0.13.0.tar.gz"
+  sha256 "f8d0a4b02c983de8a703b25480abcfc3224a0b86613b4a7ce53316657efbaa7a"
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
