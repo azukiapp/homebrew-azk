a1d692d437e6e71b5432cd427c69055e6a30b8a8
Felipe Arenales <fearenales@gmail.com>
2015-05-30 16:09:47 -0300

    Fixing URL to stage

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Azk < Formula
   homepage "http://azk.io"
-  url "http://repo.azukiapp.com/mac/azk_0.13.1.tar.gz"
+  url "http://repo-stage.azukiapp.com/mac/azk_0.13.1.tar.gz"
   sha256 "fc7a85abb6b2dfbd92eb35548a3ce7422798c8a284d290e9c0bf61d4f11231b2"
 
   depends_on :macos => :mountain_lion
