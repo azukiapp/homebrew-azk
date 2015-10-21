5f36c451e2e772a73a00961700beb6908c0bca73
Everton Ribeiro <nuxlli@gmail.com>
2015-10-21 16:20:10 -0200

    Bumping version to azk v0.16.0-rc.1+20151021.

--- Formula/azk-rc.rb
+++ Formula/azk-rc.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class AzkRc < Formula
   homepage "http://azk.io"
-  url "http://repo-stage.azukiapp.com/mac/azk_0.14.0-rc.3+20150615.tar.gz"
-  version "0.14.0-rc.3+20150615"
-  sha256 "ea9c55e7907427aceec6d743d6d69222aee58bcebe84ff0a29fe5662cc60074b"
+  url "http://repo-stage.azukiapp.com/mac/azk_0.16.0-rc.1+20151021.tar.gz"
+  version "0.16.0-rc.1+20151021"
+  sha256 "aee4f5a601f6e2aa81dbdbfbe1414cd5eb5b98ee7cee3bbbf51dffa42c4ae3d4"
   
   conflicts_with 'azukiapp/azk/azk', :because => 'installation of azk in path'
   
