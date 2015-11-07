52a71ca1636c3096e42fa5ebfe904896fc5bb471
Everton Ribeiro <nuxlli@gmail.com>
2015-11-07 16:12:20 -0200

    Bumping version to azk v0.16.1.

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -4,7 +4,7 @@ class Azk < Formula
   homepage "http://azk.io"
   url "http://repo-stage.azukiapp.com/mac/azk_0.16.1.tar.gz"
   version "0.16.1"
-  sha256 "4d94efefa7197b9531b4069aa7ce1c1ad4381fe719858047a65f9624087199d6"
+  sha256 "2d074c4f2dc32b763a5fe0123e515e654e32d61457a239a7c3a0f2302f01b663"
   
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
