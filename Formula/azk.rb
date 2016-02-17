37bf296cf676844af8c11b7e95ccf7604e5a21b7
Felipe Arenales <fearenales@gmail.com>
2016-02-17 18:15:49 -0200

    Adding support to '--HEAD' argument

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -5,14 +5,35 @@ class Azk < Formula
   url "http://repo.azukiapp.com/mac/azk_0.16.3.tar.gz"
   version "0.16.3"
   sha256 "d598be7385a594b27284e5dc051b895eaf363b6221d79c88679ff48030b2e5b7"
+  head "https://github.com/azukiapp/azk.git"
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
 
   def install
-    prefix.install Dir['*']
-    prefix.install Dir['.nvmrc']
-    prefix.install Dir['.dependencies']
+    items_path = '.'
+    if build.head?
+      ENV.deparallelize
+      ENV['HOMEBREW_TEMP']=buildpath
+      system 'make', '-e', 'package_mac'
+
+      items_path = 'package/brew/*/usr/lib/azk'
+      items = %w{ bin lib node_modules shared package.json npm-shrinkwrap.json CHANGELOG.md LICENSE README.md }
+    else
+      items = ['*']
+    end
+    items += %w{ .dependencies .nvmrc }
+
+    items.each do |item|
+      prefix.install Dir["#{items_path}/#{item}"]
+    end
+  end
+
+  def post_install
+    system 'rm -Rf ./package' if build.head?
+  end
+
+  test do
+    system "azk", "version"
   end
 end
-
