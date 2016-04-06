9dfcd1ab63e1299fdd5f747eb42b2f3b7d3a4ae7
Azuki CI <support@azukiapp.com>
2016-04-06 02:22:17 -0500

    Bumping version to azk v0.18.0-rc.1+20160406.

--- Formula/azk-rc.rb
+++ Formula/azk-rc.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class AzkRc < Formula
   homepage "http://azk.io"
-  url "http://repo-stage.azukiapp.com/mac/azk_0.16.0-rc.2+20151022.tar.gz"
-  version "0.16.0-rc.2+20151022"
-  sha256 "f4eb7c8f55d6e6f946addbb4a82990ec90659c358c01f90c3d7e316c13baaae3"
+  url "http://repo-stage.azukiapp.com/mac/azk_0.18.0-rc.1+20160406.tar.gz"
+  version "0.18.0-rc.1+20160406"
+  sha256 "6a391eea2d5e1b5a145fd45f1b301adbb729061e6e2dd7ea85d54040d8e98388"
   
   conflicts_with 'azukiapp/azk/azk', :because => 'installation of azk in path'
   
@@ -15,6 +15,7 @@ class AzkRc < Formula
     prefix.install Dir['*']
     prefix.install Dir['.nvmrc']
     prefix.install Dir['.dependencies']
+    prefix.install Dir['.package-envs']
   end
 end
 
