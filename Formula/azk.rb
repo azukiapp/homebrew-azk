7315538ce9de824d043efd3036bb38c83ee4b0cf
Felipe Arenales <fearenales@gmail.com>
2016-04-07 02:28:59 -0300

    Fixing azk formula (adding HEAD build)

--- Formula/azk.rb
+++ Formula/azk.rb
@@ -5,15 +5,36 @@ class Azk < Formula
   url "http://repo-stage.azukiapp.com/mac/azk_0.18.0.tar.gz"
   version "0.18.0"
   sha256 "23a7b4745c77673a9f4c18e37c70766626b3dfcdc0f84a35944f655f96083424"
-  
+  head "https://github.com/azukiapp/azk.git"
+
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
 
   def install
-    prefix.install Dir['*']
-    prefix.install Dir['.nvmrc']
-    prefix.install Dir['.dependencies']
-    prefix.install Dir['.package-envs']
+    items_path = '.'
+    if build.head?
+      ENV.deparallelize
+      ENV['HOMEBREW_TEMP'] = buildpath
+      system 'make', '-e', 'package_mac'
+
+      items_path = 'package/brew/*/usr/lib/azk'
+      items = %w{ bin lib node_modules shared package.json npm-shrinkwrap.json CHANGELOG.md LICENSE README.md }
+    else
+      items = ['*']
+    end
+    items += %w{ .dependencies .nvmrc .package-envs }
+
+    items.each do |item|
+      prefix.install Dir["#{items_path}/#{item}"]
+    end
+  end
+
+  def post_install
+    rmtree './package' if build.head?
+  end
+
+  test do
+    system "azk", "version"
   end
 end
 
